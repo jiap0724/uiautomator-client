fde725090bd7a79067bdd45b744f813084c24a51
xudafeng <xudafeng@126.com>
2016-06-27 14:19:15 +0800

    support unicode

--- src/com/android/uiautomator/client/cmd/SetText.java
+++ src/com/android/uiautomator/client/cmd/SetText.java
@@ -9,39 +9,46 @@ import com.android.uiautomator.client.Elements;
 import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 
+import java.nio.charset.Charset;
+
 /**
  * @author xdf
- *
  */
 public class SetText extends CommandBase {
-	@Override
-	public String execute(JSONObject args) throws JSONException {
-		try {
+    @Override
+    public String execute(JSONObject args) throws JSONException {
+        try {
 
-			String elementId = (String) args.get("elementId");
-			String text = (String) args.get("text");
+            String elementId = (String) args.get("elementId");
+            String text = (String) args.get("text");
 
-			Element element = Elements.getGlobal().getElement(elementId);
+            Element element = Elements.getGlobal().getElement(elementId);
 
-			boolean needPressEnter = false;
-			
-			if (text.endsWith("\\n")) {
-				needPressEnter = true;
-				text = text.replace("\\n", "");
-			}
+            boolean needPressEnter = false;
 
-			boolean result = element.setText(text);
+            if (text.endsWith("\\n")) {
+                needPressEnter = true;
+                text = text.replace("\\n", "");
+            }
 
-			if (needPressEnter) {
-				final UiDevice d = UiDevice.getInstance();
-				d.pressEnter();
-			}
+            Charset M_UTF7 = Charset.forName("x-IMAP-mailbox-name");
+            Charset ASCII = Charset.forName("US-ASCII");
 
-			return success(result);
-		} catch (final UiObjectNotFoundException e) {
-			return failed("NoSuchElement");
-		} catch (final Exception e) {
-			return failed("UnknownError");
-		}
-	}
+            byte[] encoded = text.getBytes(M_UTF7);
+            String str = new String(encoded, ASCII);
+
+            boolean result = element.setText(str);
+
+            if (needPressEnter) {
+                final UiDevice d = UiDevice.getInstance();
+                d.pressEnter();
+            }
+
+            return success(result);
+        } catch (final UiObjectNotFoundException e) {
+            return failed("NoSuchElement");
+        } catch (final Exception e) {
+            return failed("UnknownError");
+        }
+    }
 }
