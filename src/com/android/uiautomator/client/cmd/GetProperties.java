8c2a4a4fddfc7d7ef846135bec81bddfe99bb187
xudafeng <xudafeng@126.com>
2016-07-20 12:08:56 +0800

    support get props

---
+++ src/com/android/uiautomator/client/cmd/GetProperties.java
@@ -0,0 +1,35 @@
+package com.android.uiautomator.client.cmd;
+
+import android.graphics.Rect;
+
+import com.android.uiautomator.client.CommandBase;
+import com.android.uiautomator.client.Element;
+import com.android.uiautomator.client.Elements;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @author xdf
+ *
+ */
+public class GetProperties extends CommandBase {
+    @Override
+    public String execute(JSONObject args) throws JSONException {
+        try {
+            String elementId = (String) args.get("elementId");
+            Element el = Elements.getGlobal().getElement(elementId);
+            final Rect rect = el.element.getVisibleBounds();
+            JSONObject size = new JSONObject();
+            size.put("width", rect.width());
+            size.put("height", rect.height());
+            size.put("centerX", rect.centerX());
+            size.put("centerY", rect.centerY());
+            return success(size.toString());
+        } catch (final UiObjectNotFoundException e) {
+            return failed("NoSuchElement");
+        } catch (final Exception e) {
+            return failed("UnknownError");
+        }
+    }
+}
