6537561d576549b0883bbbd6fca3193a70e05e18
xudafeng <xudafeng@126.com>
2016-11-17 12:01:52 +0800

    tweak clear

--- src/com/android/uiautomator/client/cmd/ClearText.java
+++ src/com/android/uiautomator/client/cmd/ClearText.java
@@ -4,6 +4,7 @@ import com.android.uiautomator.client.CommandBase;
 import com.android.uiautomator.client.Element;
 import com.android.uiautomator.client.Elements;
 import com.android.uiautomator.client.Status;
+import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,8 +19,12 @@ public class ClearText extends CommandBase {
         try {
             String elementId = (String) args.get("elementId");
             Element el = Elements.getGlobal().getElement(elementId);
-
             el.clearText();
+            UiDevice.getInstance().clearLastTraversedText();
+            String text = el.getText();
+            for (int i=0;i<text.length();i++) {
+                UiDevice.getInstance().pressDelete();
+            }
             return success(true);
         } catch (final UiObjectNotFoundException e) {
             return failed(Status.NoSuchElement);
